8b3a7fe7ebb0aeffdc28d4543fff9906b66e3b0a
musasyncs <rubato.cw@gmail.com>
2021-07-18 17:27:55 +0800

    0718 17:30commit

---
+++ WoodenHouse/MenuTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuTableViewController.swift
+//  WoodenHouse
+//
+//  Created by Ewen on 2021/7/17.
+//
+
+import UIKit
+
+class MenuTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //backBarButtonItem 不要有標題
+        let menuBackBarButtton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        navigationItem.backBarButtonItem = menuBackBarButtton
+    }
+}
